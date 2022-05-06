0484728484b016226b8f072a8acb3668fca4309d
leprofessionnel79 <jannblomondo@gmail.com>
2022-05-06 21:53:32 +0300

    say hello

--- SDL_OPENGL/main.cpp
+++ SDL_OPENGL/main.cpp
@@ -52,6 +52,8 @@ void display()
 
 int main(int argc,char** argv)
 {
+	cout << "hello ahmad" << endl;
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Surface* screen=SDL_SetVideoMode(640,480,32,SDL_SWSURFACE|SDL_OPENGL);
 	bool running=true;
